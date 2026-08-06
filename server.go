7763eab3dcf09fc3b24ea8e350228c5f1322ff56
zack-fathi <zackeryfathi@gmail.com>
2026-08-05 20:04:33 -0400

    extending server build to now parse bulk string and array messages seperately. Can identify them now but still implementing parsing with reader correctly.

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	// "fmt"
 	"fmt"
 	"net"
+
 	// "time"
 	// "sync/atomic"
 	"log"
@@ -31,12 +32,9 @@ func listen() {
 
 }
 
-
-
-
 func handle(conn net.Conn) {
 
-	fmt.Println("Handling connection\n")
+	fmt.Println("Handling connection")
 
 	// make a buffer
 	// while there are still bytes to read
@@ -48,13 +46,18 @@ func handle(conn net.Conn) {
 
 	for {
 
-		_, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Connection closed")
-			return
+		byteType := readValue(reader)
+
+		var msg string
+		if byteType == '*' {
+			msg = parseArray(reader)
+		} else if byteType == '$' {
+			msg = parseBulk(reader)
 		}
 
-		_, err = conn.Write([]byte("+OK\r\n"))
+		fmt.Println(msg)
+
+		_, err := conn.Write([]byte("+OK\r\n"))
 		if err != nil {
 			fmt.Println("Connection closed")
 			return
@@ -62,4 +65,40 @@ func handle(conn net.Conn) {
 
 	}
 
+}
+
+// Reads first byte from stream to determine data type
+func readValue(reader *bufio.Reader) byte {
+
+	byte_type, err := reader.ReadByte()
+	if err != nil {
+		fmt.Println("Connection closed")
+		return 'X'
+	}
+
+	return byte_type
+}
+
+// Parses an array message
+func parseArray(reader *bufio.Reader) string {
+
+	fmt.Println("You have read a array")
+	msg, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Connection closed")
+		return ""
+	}
+	return msg
+}
+
+// Parses a bulk string message
+func parseBulk(reader *bufio.Reader) string {
+
+	fmt.Println("You have read a bulk string")
+	msg, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Connection closed")
+		return ""
+	}
+	return msg
 }
